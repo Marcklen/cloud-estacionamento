c22e244665e4a5154824387dbe302e76666cedcb
Marcklen <marcklen@icloud.com>
2022-07-26 12:01:45 -0300

    estrutura inicial

---
+++ src/main/java/br/com/dio/estacionamento/CloudEstacionamentoApplication.java
@@ -0,0 +1,12 @@
+package br.com.dio.estacionamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudEstacionamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudEstacionamentoApplication.class, args);
+	}
+}
---
+++ src/test/java/br/com/dio/estacionamento/CloudEstacionamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dio.estacionamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudEstacionamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
